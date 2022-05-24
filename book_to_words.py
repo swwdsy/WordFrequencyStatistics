75b5932a46a0c5bd1e9dff476ef9dd3bfa5b0cc9
wjw <wjw2411@163.com>
2022-05-24 21:13:54 +0800

    更新

--- book_to_words.py
+++ book_to_words.py
@@ -4,8 +4,7 @@ from nltk.tokenize import word_tokenize
 import pandas as pd
 
 
-
-# 对字符串进行分词,识别词性，根据词性还原词根
+# 对字符串进行分词,识别词性，根据词性还原单词原型
 def lemmatize_all(text):
     wnl = WordNetLemmatizer()
     for word, tag in pos_tag(word_tokenize(text)):
@@ -22,9 +21,10 @@ def lemmatize_all(text):
         else:
             yield word.lower().lower()
 
+
 def get_words_list(book):
     # 将文件读取为字符串
-    with open(book,'r',encoding = 'UTF-8') as file:
+    with open(book, 'r', encoding='UTF-8') as file:
         string_book = file.read()
     # 对字符串进行分词,词性还原
     words_list = []
@@ -32,19 +32,24 @@ def get_words_list(book):
         words_list.append(word)
     return words_list
 
+
 def save_words_as_csv(book):
     words_list = get_words_list(book)
 
     #频率由高到低排列
     words_sort_by_freq = pd.value_counts(words_list)
-    dict_word = {'word':words_sort_by_freq.index, 'count':words_sort_by_freq.values}
+    dict_word = {
+        'word': words_sort_by_freq.index,
+        'count': words_sort_by_freq.values
+    }
     words = pd.DataFrame(dict_word)
 
     #清除有标点符号行,字母小于二的行
-    words = words[~words['word'].str.contains('~|\|`|!|@|#|\$|%|\^|&|\*|\(|\)|-|_|\+|=|{|\[|}|]|:|;|"|\'|<|,|>|\.|/|\?')]
-    words = words[~(words['word'].str.len()<3)]
-    words.reset_index(drop=True,inplace=True)
-    
+    words = words[~words['word'].str.contains(
+        '~|\|`|!|@|#|\$|%|\^|&|\*|\(|\)|-|_|\+|=|{|\[|}|]|:|;|"|\'|<|,|>|\.|/|\?|1|2|3|4|5|6|7|8|9|0'
+    )]
+    words = words[~(words['word'].str.len() < 3)]
+    words.reset_index(drop=True, inplace=True)
 
-    words.to_csv('output/words.csv', index=False)
-    print('Save as output/words.csv')
+    words.to_csv('output/words_rank.csv', index=False)
+    print('Save as output/words_rank.csv')
--- main.py
+++ main.py
@@ -1,35 +1,49 @@
 import pandas as pd
+
 import book_to_words
 
-# 修改书名
-file_book = 'hp1ch1'
-file_dict = 'ky'
+# 书名(统计目标)
+file_name = '2001真题阅读.txt'
+
+# 字典
+dict_name = 'postgratuate.csv'
+
 # 获取全书所有单词列表存为csv文件
-book_to_words.save_words_as_csv('book/' + file_book + '.txt')
+book_to_words.save_words_as_csv('book/' + file_name)
 
 # 读取整理好的words 和 dict
-file_words = 'output/words.csv'
+file_words = 'output/words_rank.csv'
+
 words = pd.read_csv(file_words, index_col=0)
-words[file_dict] = False
-dict = pd.read_csv('dictionary/'+ file_dict + '.csv') 
+words[dict_name] = False
+
+dict = pd.read_csv('dictionary/' + dict_name)
 
 # 判断是否为dict单词
 words_len = len(words)
+
 for i in range(0, len(dict)):
-    try: 
-        words.loc[dict['word'][i], file_dict] = True
+    try:
+        words.loc[dict['word'][i], dict_name] = True
     except:
         break
     else:
         len = len
+
 words = words.head(words_len)
 
 # 判断 书中dict单词 占 全部dict单词的百分比
-number_of_words_in_dict = len(words.loc[words[file_dict] == True])
+
+number_of_words_in_dict = len(words.loc[words[dict_name] == True])
+
 number_of_dict = len(dict)
-output = file_book + "中的" + file_dict + "单词 占 所有" + file_dict + "单词 的" + str(round((number_of_words_in_dict/number_of_dict*100), 2)) + '%'
+
+output = file_name + "中的" + dict_name + "单词 占 所有" + dict_name + "单词 的" + str(
+    round((number_of_words_in_dict / number_of_dict * 100), 2)) + '%'
 print(output)
 
 words.to_csv('output/words_result.csv')
+
 # words.loc[words[file_dict] == True, ['word', 'count']].to_csv('output\word_in_dict.csv')
+
 # words.loc[words[file_dict] == False, ['word', 'count']].to_csv('output\word_without_in_dict.csv')
